f8ec7963eaa6bee57ca0d0d9d76b8f6c8b4aae26
Pablo Santiago <pablosantiagolopez1@gmail.com>
2022-03-27 16:08:59 +0200

    Fix in Rekono user creation

--- rekono/utils/linux/users.py
+++ rekono/utils/linux/users.py
@@ -1,11 +1,14 @@
 import subprocess
 
-from rekono.config import REKONO_USER
+from rekono.config import REKONO_HOME_DIRECTORY, REKONO_USER
 
 
 def create_rekono_user() -> None:
     '''Create Rekono local user to execute the Rekono services.'''
-    subprocess.run(['sudo', 'useradd', REKONO_USER], capture_output=True)
+    subprocess.run(
+        ['sudo', 'useradd', '-r', REKONO_USER, '--shell=/usr/sbin/nologin', f'--home={REKONO_HOME_DIRECTORY}'],
+        capture_output=True
+    )
 
 
 def remove_rekono_user() -> None:
